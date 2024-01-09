5923c35f9bdf404f0cafd8d7603d6139186f701a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-01-09 19:22:13 +0530

    Update the security

--- day-2/security/basic-auth/src/main/java/lk/ijse/dep11/security/security/SecurityInterceptor.java
+++ day-2/security/basic-auth/src/main/java/lk/ijse/dep11/security/security/SecurityInterceptor.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep11.security.security;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,37 +12,32 @@ public class SecurityInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String endpoint = request.getServletPath();
+        if (endpoint.equalsIgnoreCase("/error")) return true;
 
-        // (1)
+        // (1) Check for the authorization header
         String authorization = request.getHeader("Authorization");
         if (authorization != null){
-
-            // (2)
+            // (2) Check for the basic authentication
             if (authorization.toUpperCase().startsWith("BASIC")){
-
-                // (3)
+                // (3) Extract the encoded text
                 String encodedText = authorization.substring(6);
-
-                // (4)
+                // (4) Decode the credentials
                 byte[] decodedTextBytes = Base64.getDecoder().decode(encodedText);
                 String decodedText = new String(decodedTextBytes);
-
-                // (5)
+                // (5) Extract username and password
                 String[] credentials = decodedText.split(":");
                 if (credentials.length == 2){
-
-                    // (6)
                     String username = credentials[0];
                     String password = credentials[1];
-
+                    // (6) Verify user
                     if (username.equals("ijse") && password.equals("dep11")){
                         return true;
                     }
                 }
             }
         }
-        response.sendError(401);
-        return false;
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
     }
 
 }
